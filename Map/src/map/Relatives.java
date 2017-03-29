e7b55cd398561841104829d5e62d1de92e376933
17AT0666 <17AT0666@HSS-101-01.westwindsor.k12.nj.us>
2017-03-29 13:00:04 -0400

    Worked on Relatives

--- Map/src/map/Relatives.java
+++ Map/src/map/Relatives.java
@@ -13,37 +13,53 @@ public class Relatives
 
 	public Relatives()
 	{
-		
-
+		map = new TreeMap<String,Set<String>>();
 	}
 
 	public void setPersonRelative(String line)
 	{
+		Set<String> temp = new TreeSet<String>();
 		String[] personRelative = line.split(" ");
-		
-		
-
-
+		//System.out.println(personRelative[0]);
+		//System.out.println(personRelative[1]);
+		if(map.containsKey(personRelative[0])){
+				temp=map.get(personRelative[0]);
+				temp.add(personRelative[1]);
+				//System.out.println(temp);
+				map.replace(personRelative[0], temp);
+		}
+		else{
+			map.put(personRelative[0], new TreeSet<String>());
+			map.get(personRelative[0]).add(personRelative[1]);
+		}
+		if(map.containsKey(personRelative[1])){
+			temp=map.get(personRelative[1]);
+			//System.out.println(temp);
+			temp.add(personRelative[0]);
+			//System.out.println(temp);
+			map.replace(personRelative[1], temp);
+		}
+		else{
+			map.put(personRelative[0], new TreeSet<String>());
+			map.get(personRelative[0]).add(personRelative[1]);
+		}
 	}
-
 
 	public String getRelatives(String person)
 	{
-		return "";
+		return map.get(person).toString();
 	}
-
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
-
-
+		Set<String> keys = map.keySet();
+		for(String key : keys){
+			//System.out.println(map.get(key));
+			output+="\n" + key + " is related to " + map.get(key);
+		}
 		return output;
+		//System.out.println(map);
 	}
+	
 }
--- Map/src/map/RelativesRunner.java
+++ Map/src/map/RelativesRunner.java
@@ -9,6 +9,12 @@ public class RelativesRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		//add more test cases		
+		//add more test cases	
+		Relatives relatives = new Relatives();
+		Scanner file = new Scanner(new File("H:/Eclipse Workspace/Map/src/map/relatives.dat")); //C:/Users/17at0666/Downloads/spantoeng.dat
+		while(file.hasNextLine()){
+			relatives.setPersonRelative(file.nextLine());
+		}
+		System.out.println(relatives);
 	}
 }
