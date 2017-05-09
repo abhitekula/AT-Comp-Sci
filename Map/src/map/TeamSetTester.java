25dd926171237213cece7aeadaf3ff7c845f4b4e
Abhinav <abhitekula@gmail.com>
2017-05-09 09:08:08 -0400

    Finished Wrestler Joe

--- Map/src/map/TeamSetTester.java
+++ Map/src/map/TeamSetTester.java
@@ -4,6 +4,15 @@ public class TeamSetTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TeamSet team = new TeamSet();
+		team.add(new Wrestler("Abhi", 200));
+		team.add(new Wrestler("Joe", 150));
+		team.add(new Wrestler("Bob", 120));
+		team.add(new Wrestler("Jon", 100));
+		team.add(new Wrestler("Henry", 110));
+		
+		System.out.println("Average Weight: " + team.avgWeight());
+		System.out.println("LightWeights: " + team.lightweights());
 		
 	}
 
