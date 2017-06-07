40d079fb1ad19d29a0ffa39d1e934b1ce0495984
17AT0666 <17AT0666@HSS-101-01.westwindsor.k12.nj.us>
2017-06-07 11:46:33 -0400

    Finished Binary Search Tree

--- Binary Search Trees/src/tree/BSTreeRunner.java
+++ Binary Search Trees/src/tree/BSTreeRunner.java
@@ -8,12 +8,71 @@ public class BSTreeRunner
    {
  		//add test cases here
  		
+ 		BinarySearchTree tree = new BinarySearchTree();
  		
- 		
- 		
- 		
- 		
- 		
+ 		tree.add(10);
+ 		tree.add(9);
+ 		tree.add(8);
+ 		tree.add(7);
+ 		tree.add(6);
+ 		tree.add(5);
+ 		tree.add(4);
  
+ 		//System.out.println(tree);
+ 		
+ 		System.out.println("\nPre-Order:");
+ 		tree.preOrder();
+ 		
+ 		System.out.println("\nPost-Order:");
+ 		tree.postOrder();
+ 		
+ 		System.out.println("\nIn-Order:");
+ 		System.out.println(tree);;
+ 		
+ 		System.out.println("\nRev-Order:");
+ 		tree.revOrder();
+ 		
+ 		System.out.println("\nHeight:");
+ 		System.out.println(tree.getHeight());
+ 		
+ 		System.out.println("\nWidth:");
+ 		System.out.println(tree.getWidth());
+ 		
+ 		System.out.println("\nNum Leaves:");
+ 		System.out.println(tree.getNumLeaves());
+ 		
+ 		System.out.println("\nNum Levels:");
+ 		System.out.println(tree.getNumLevels());
+ 		
+ 		System.out.println("\nNum Nodes:");
+ 		System.out.println(tree.getNumNodes());
+ 		
+ 		System.out.println("\nIs Full???????");
+ 		System.out.println(tree.isFull());
+ 		
+ 		System.out.println("\nSearch (10) :");
+ 		System.out.println(tree.search(10));
+ 		
+ 		System.out.println("\nMax Node:");
+ 		System.out.println(tree.maxNode());
+ 		
+ 		System.out.println("\nMin Node:");
+ 		System.out.println(tree.minNode());
+ 		
+ 		System.out.println("\nLevel Order");
+// 		System.out.println(tree.levelOrder());
+ 		tree.levelOrder();
+ 		
+ 		System.out.println("\n\nRemove (10):");
+ 		tree.remove(10);
+ 		System.out.println(tree);
+ 		
+ 		System.out.println("\n\nRemove (9):");
+ 		tree.remove(9);
+ 		System.out.println(tree);
+ 		
+ 		System.out.println("\n\nRemove (8):");
+ 		tree.remove(8);
+ 		System.out.println(tree);
    }
 }
--- Binary Search Trees/src/tree/BinarySearchTree.java
+++ Binary Search Trees/src/tree/BinarySearchTree.java
@@ -11,6 +11,10 @@ public class BinarySearchTree
 	{
 		root = null;
 	}
+	
+	private TreeNode getRoot(){
+		return root;
+	}
 
 	public void add(Comparable val)
 	{
@@ -20,7 +24,7 @@ public class BinarySearchTree
 	private TreeNode add(Comparable val, TreeNode tree)
 	{
 	   if(tree == null)
-			tree = new TreeNode(val);
+			return tree = new TreeNode(val);
 		
 		Comparable treeValue = tree.getValue();
 		int dirTest = val.compareTo(treeValue);
@@ -32,24 +36,68 @@ public class BinarySearchTree
 			tree.setRight(add(val, tree.getRight()));
 		
 		return tree;
+		
 	}
 
-   public void inOrder()
+	String output;
+   public String inOrder()
 	{
-		inOrder(root);
-		System.out.println("\n\n");
+	   	output="";
+	   	System.out.println("\n");
+		return inOrder(root);
 	}
 
-	private void inOrder(TreeNode tree)
+	private String inOrder(TreeNode tree)
 	{
 		if (tree != null){
 			inOrder(tree.getLeft());
-			System.out.print(tree.getValue() + " ");
+			output+=tree.getValue() + " ";
 			inOrder(tree.getRight());
 		}
+		return output;
 	}
 
 	//add preOrder, postOrder, and revOrder
+	
+	public void preOrder() {
+		preOrder(root);
+		System.out.println("\n\n");
+	}
+
+	private void preOrder(TreeNode tree) {
+		if (tree != null) {
+			System.out.print(tree.getValue() + " ");
+			preOrder(tree.getLeft());
+			preOrder(tree.getRight());
+		}
+	}
+
+	public void postOrder() {
+		postOrder(root);
+		System.out.println("\n\n");
+	}
+
+	private void postOrder(TreeNode tree) {
+		if (tree != null) {
+			postOrder(tree.getLeft());
+			postOrder(tree.getRight());
+			System.out.print(tree.getValue() + " ");
+		}
+	}
+
+	public void revOrder() {
+		revOrder(root);
+		System.out.println("\n\n");
+	}
+
+	private void revOrder(TreeNode tree) {
+		if (tree != null) {
+			revOrder(tree.getRight());
+			System.out.print(tree.getValue() + " ");
+			revOrder(tree.getLeft());
+		}
+	}
+
 	
 	
 	public int getNumLevels()
@@ -63,49 +111,161 @@ public class BinarySearchTree
 			return 0;
 		else if(getNumLevels(tree.getLeft())>getNumLevels(tree.getRight()))
 			return 1+getNumLevels(tree.getLeft());
-		else
-			return 1+getNumLevels(tree.getRight());
+		return 1+getNumLevels(tree.getRight());
 	}
 
 
 
 	//add getNumLeaves, getWidth, getHeight, getNumNodes, and isFull
+	
+	public int getNumNodes(){
+		return getNumNodes(root);
+	}
+	
+	public int getNumNodes(TreeNode tree){
+		if(tree==null)
+			return 0;
+		else
+			return 1 + getNumNodes(tree.getLeft()) + getNumNodes(tree.getRight());
+	}
 
+	public int getNumLeaves() {
+		return getNumLeaves(root);
+	}
 
+	private int getNumLeaves(TreeNode tree) {
+		if (tree != null) {
+			if (tree.getLeft() == null && tree.getRight() == null)
+				return 1;	
+			return getNumLeaves(tree.getLeft()) + getNumLeaves(tree.getRight());
+		}
+		return 0;
+	}
 
+	int count;
+	public int getHeight() {
+		count=0;
+		return getHeight(root);
+	}
 
+	private int getHeight(TreeNode tree) {
+		while(tree.getLeft()!=null){
+			tree=tree.getLeft();
+			count++;
+		}
+		return count;
+	}
 
+	public int getWidth() {
+		return getNumLevels(root.getLeft()) + getNumLevels(root.getRight()) + 1;
+	}
 
+	public boolean isFull() {
+		return isFull(root);
+	}
 
-
-
-
-
-
-
+	private boolean isFull(TreeNode tree) {
+		if(getNumNodes()==Math.pow(2, getNumLevels())-1)
+			return true;
+		return false;
+	}
 
 	
-	
+	public boolean search(Comparable x) {
+		return search(x, root);
+	}
 	
 	//search
+	private boolean search(Comparable x, TreeNode tree) {
+		if(root.getValue().equals(x))
+			return true;
+		if (tree != null){
+			if ((tree.getLeft() != null && tree.getLeft().getValue().equals(x)) || (tree.getRight() != null && tree.getRight().getValue().equals(x)))
+				return true;
+			return search(x, tree.getLeft()) || search(x, tree.getRight());
+		}
+		return false;
+	}
+
 	
 	//maxNode
 	
+	public Comparable maxNode() {
+		return maxNode(root);
+	}
+
+	public Comparable maxNode(TreeNode tree) {
+		Comparable max=root.getValue();
+		while(tree.getRight()!=null)
+			tree=tree.getRight();
+		if(tree.getValue().compareTo(root.getValue())<1)
+			return tree.getValue();
+		else
+			return root.getValue();
+	}
+	
 	//minNode
 	
+	public Comparable minNode() {
+		return minNode(root);
+	}
+	
+	public Comparable minNode(TreeNode tree) {
+		while(tree.getLeft()!=null)
+			tree=tree.getLeft();
+		if(tree.getValue().compareTo(root.getValue())<1)
+			return tree.getValue();
+		else
+			return root.getValue();
+	}
+	
 	//level order
+	
+	LinkedList<TreeNode> list;
+	public void levelOrder(){
+		list = new LinkedList<TreeNode>();
+		levelOrder(root);
+	}
+	
+	
+	public void levelOrder(TreeNode tree) {
+		if (tree != null) {
+			list.add(tree);
+			System.out.print(tree.getValue() + " ");
+			levelOrder(tree.getLeft());
+			levelOrder(tree.getRight());
+		}
+		//System.out.println(list);
+	}
 	
 	//display like a tree
 	
 	//remove
 
+	BinarySearchTree a;
+	public void remove(Comparable x){
+		a = null;
+		a = new BinarySearchTree();
+		remove(x,root);
+	}
+	
+	public void remove(Comparable x, TreeNode tree) {
+		if(tree!=null){
+			if(!tree.getValue().equals(x))
+				a.add(tree.getValue());
+			remove(x, tree.getLeft());
+			remove(x, tree.getRight());
+		}
+		else
+			this.root = a.getRoot();
+	}
 
 	public String toString()
 	{
-		return "";
+		return inOrder();
 	}
 
-	private String toString(TreeNode tree)
+	public String toString(TreeNode tree)
 	{
 		return "";
 	}
