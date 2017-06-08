26265ebe69e1a72ff48f2f706d499d3519aad2b7
17AT0666 <17AT0666@HSS-101-01.westwindsor.k12.nj.us>
2017-06-08 13:08:21 -0400

    Added graph project

---
+++ Graph/src/graph/GraphRunner.java
@@ -0,0 +1,20 @@
+package graph;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class GraphRunner
+{
+	public static void main( String[] args ) throws IOException
+	{
+		Scanner file = new Scanner(new File("graph1.dat"));
+		int howManyTimes = file.nextInt();
+		file.nextLine();
+		for(int x=0; x<howManyTimes; x++)
+		{
+			//add code here
+		}
+	}
+}
